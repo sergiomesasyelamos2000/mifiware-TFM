03c66fc49f1b73ed5c8a242d2c1466fa2b910d9e
Sergio <mesasyelamos@gmail.com>
2024-05-11 13:49:36 +0200

    feat: añado comandos de build de la aplicación y aumento el bundle

--- apps/mifiware-tfm/src/environments/environment.prod.ts
+++ apps/mifiware-tfm/src/environments/environment.prod.ts
@@ -1,3 +1,23 @@
+import { LANGUAGES_ENUM } from '@mifiware-tfm/entity-data-models';
+
+const url = 'http://localhost:3000';
+
 export const environment = {
   production: true,
+  environment: 'PROD',
+  enableCryptoStorage: false,
+  publicAssetsUrl: `${url}`,
+  apiUrl: `${url}/api/v1`,
+  disableConsoleLog: false,
+  sentry: {
+    enable: false,
+    dsn: '',
+    attachStacktrace: false,
+    tracesSampleRate: 0,
+  },
+  // Transloco config
+  transloco: {
+    defaultLang: LANGUAGES_ENUM.es,
+    availableLangs: Object.keys(LANGUAGES_ENUM),
+  },
 };

2830b0467a1e3cc1ef1986b3eda65c386a78b65c
Sergio <mesasyelamos@gmail.com>
2024-06-20 19:06:09 +0200

    fix: modifico home

--- apps/mifiware-tfm/src/app/modules/home/home.module.ts
+++ apps/mifiware-tfm/src/app/modules/home/home.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './views/home.component';
 import { TranslocoModule } from '@ngneat/transloco';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, TranslocoModule],
+  imports: [CommonModule, HomeRoutingModule, TranslocoModule, ButtonModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
